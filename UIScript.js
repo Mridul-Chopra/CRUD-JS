1f0318d94a30545945bf15ae54e567b957f05fad
Mridul-Chopra <59679184+Mridul-Chopra@users.noreply.github.com>
2020-01-26 21:40:02 +0530

    Add files via upload

--- UIScript.js
+++ UIScript.js
@@ -2,50 +2,7 @@
 
 var records_=[];
 
-function addRow()
-{
-    let table = document.getElementById("records");
-
-    let row  = table.insertRow(table.length);
-
-    
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-         
-    cell1.setAttribute("contenteditable", true);
-    cell2.setAttribute("contenteditable", true);
-    cell3.setAttribute("contenteditable", true);
-    cell4.setAttribute("contenteditable", true);
-
-  
-
-    cell5.innerHTML = ' <i class="fa fa-minus-square" style="font-size:2em;color:red" onclick=deleteRow(this)></i>';
-    cell5.id = "row-options";
-
-    showOrHide();
-
-}
-
-function deleteRow(e)
-{
-    let row = e.parentNode.parentNode;
-    let rowIndex = e.parentNode.parentNode.rowIndex;
-
-   records_.splice(rowIndex-1,1)
-
-  records_.filter( x=> x.id>rowIndex).map( x=> x.id = x.id -1);
-   
-    console.log(records_);
-    row.remove();
-    
-    showOrHide();
-}
-
-
-class Student
+class Student  // this class contains student's information
 {
 
    constructor(data , index)
@@ -65,26 +22,71 @@ class Student
 }
 
 
+function addRow()  // function to add new rows to the table
+{
+    let table = document.getElementById("records");  // getting table 
 
-function saveData()
+    let row  = table.insertRow(table.length);  // inserting row
+
+    // inserting cells
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+	
+    // setting properties of cell     
+    cell1.setAttribute("contenteditable", true);
+    cell2.setAttribute("contenteditable", true);
+    cell3.setAttribute("contenteditable", true);
+    cell4.setAttribute("contenteditable", true);
+
+  
+
+    cell5.innerHTML = ' <i class="fa fa-minus-square" style="font-size:2em;color:red" onclick=deleteRow(this)></i>';
+    cell5.id = "row-options";
+
+    showOrHide();  // checking if to show or hide save button
+
+}
+
+function deleteRow(e)  // function to delete rows
+{  
+    let row = e.parentNode.parentNode;  // getting rows
+    let rowIndex = e.parentNode.parentNode.rowIndex;  
+
+   records_.splice(rowIndex-1,1)  // removing record
+
+  records_.filter( x=> x.id>rowIndex).map( x=> x.id = x.id -1);  // updating ids
+   
+    console.log(records_);  
+    row.remove();  //removing row
+    
+    showOrHide();  // function to check if to show or hide save button
+}
+
+
+
+
+
+function saveData()  // function for saving data 
 {
     let table = document.getElementById("records");
     let row;
 
-    let message = "Records inserted sucessfully";
+    let message = "Records inserted sucessfully";  // feedback message
 
     let data =[];
-    let isFilled = true;
+    let isFilled = true;  // checks if all data is filled
+	
+	records_.splice(0,records_.length);
 
+	// iterating all rows to get data
     for( i = 1; i<table.rows.length ; i++)
     {
         data.splice(0, 4);
         row = table.rows[i];
 
-        if(isAlreadyInserted(i))
-        {
-            continue;
-        }
 
         for(j=0;j<=3;j++)
         {
@@ -92,6 +94,7 @@ function saveData()
              let info = cell.innerHTML ;
              if(info== "")
              {
+				 // changing cell properties to highlight the blank fields
                 cell.style.backgroundColor = "#ffcccc";
                 isFilled = false;
                 message = "Please fill the highighted fields to insert all records"
@@ -106,7 +109,7 @@ function saveData()
         }
 
        
-
+	// inserting if filled
         if(isFilled)
         { new Student(data , i); }
 
@@ -114,37 +117,31 @@ function saveData()
     }
 
     console.log(records_);
-    showMessage(message);
+    showMessage(message);  // showing alert box
 }
 
 
-function isAlreadyInserted(index)
-{
-    
-  return  records_.filter( x=> x.id == index).length == 0 ? false : true;
-    
-}
 
-function closeMessage()
+function closeMessage()  //function to close alert
 {
     var messageBox = document.getElementById("messageBox");
     messageBox.style.marginTop = "-30em";
 }
 
-function showMessage(message)
+function showMessage(message) // function to close alert
 {
     var messageBox = document.getElementById("messageBox");
     document.getElementById("message-content").innerHTML = message
     messageBox.style.marginTop = "-1em";
 }
 
-function showOrHide()
+function showOrHide()  // decides whether to show or hide save button
 {
 
     let button = document.getElementById("saveButton");
     let table =  document.getElementById("records");
     
-    if(table.rows.length > 1)
+    if(table.rows.length > 1)  // show if table has rows
     button.style.display = "block";
     else
     button.style.display = "none";
@@ -154,20 +151,22 @@ function showOrHide()
 }
 
 
-function showSearchBox()
+function showSearchBox()  // function to view search box
 {
     var messageBox = document.getElementById("search-box");
     messageBox.style.marginTop = "0em";
 
 }
 
-function search()
+function search()    // function to search for required records
 {
     let name = document.getElementById("search-content").value;
     console.log("Name is : "+ name);
-    let student = records_.filter(x=> x.name == name);
-    console.log(records_);
-    let message = " Name : "+student.name +"/n"+ "Branch : "+student.branch+"\n Roll No. : "+student.rollNo+"\n Percentage : "+student.percent;
+    let student = records_.filter(x=> x.name == name);  // getting record with same name as input
+    console.log(student);
+	
+	// printing the message
+    let message = " Name : "+student[0].name +"<br>"+ "Branch : "+student[0].branch+"<br> Roll No. : "+student[0].rollNo+"<br> Percentage : "+student[0].percent;
     
     var messageBox = document.getElementById("search-box");
     messageBox.style.marginTop = "-30em";
